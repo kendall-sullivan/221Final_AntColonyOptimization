c3fb2bea3ee07e1f49c63106a183199f269cf0e3
kendall-sullivan <123098060+kendall-sullivan@users.noreply.github.com>
2024-05-03 19:49:58 -0500

    implementation

--- src/AntColonyOptimization.java
+++ src/AntColonyOptimization.java
@@ -1,10 +1,14 @@
 package src;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.Objects;
 import java.util.Random;
 
 import src.Comp128Graph.AdjacencyListGraph;
+import src.Comp128Graph.Bag;
 
 /**
  * Ant Colony Optimization Implementation
@@ -14,47 +18,63 @@ public class AntColonyOptimization {
     private Random rand = new Random();
     
     public List<Edge> antColonyOptimization(AdjacencyListGraph G, double c, int n, int m, double a, double b, double rho) {
-        
         List<Edge> bestPath = null;
         double bestPathCost = Double.MAX_VALUE;
         int count = 0;
         this.initializePheromoneLevels(G, count);
         while (!this.terminated) {
-            List<List<Edge>> allPaths = new ArrayList<>();
             for (int k = 0; k < m; k++) {
-                List<Edge> path = this.oneAntsPath(G, a, b);
+                HashMap<List<Edge>, Double> pathAndCost = this.oneAntsPath(G, a, b);
+                double pathCost = 0;
+                List<Edge> path = null;
+                for (double value : pathAndCost.values()) {
+                    pathCost = value;
+                }
+                for (List<Edge> key : pathAndCost.keySet()) {
+                    path = key;
+                }
+                if (bestPath == null || pathCost < bestPathCost) {
+                    bestPath = path;
+                    bestPathCost = pathCost;
+                }
+            }
+            updatePheromones(G, rho);
+            if (count == n) {
+                break;
+            } else {
+                count++;
             }
         }
-
         return bestPath;
     }
 
 
-    public void initializePheromoneLevels(AdjacencyListGraph G, int c) {
+    private void initializePheromoneLevels(AdjacencyListGraph G, int c) {
         for (Edge edge : G.getEdges()) {
             edge.setPheromoneLevel(c);
         }
     }
 
-// create a path for an ant 
-    public List<Edge> oneAntsPath(AdjacencyListGraph G, double a, double b) {
+    // create a path for an ant 
+    private HashMap<List<Edge>, Double> oneAntsPath(AdjacencyListGraph G, double a, double b) {
         List<Edge> path = new ArrayList<>();
         double pathTotalCost = 0;
-        int randomVertex = this.rand.nextInt(G.getAdj().length);
+        int current = this.rand.nextInt(G.getAdj().length);
         boolean[] visited = new boolean[G.getAdj().length];  
-        visited[randomVertex] = true;
+        visited[current] = true;
 
         while (this.notAllVisited(visited)) {
-            double[] probabilities = this.getProbabilities(randomVertex, G, visited, a, b);
+            HashMap<Edge, Double> probabilities = this.getProbabilities(current, G, visited, a, b);
             Edge next = this.chooseNextVertex(probabilities);  
             if (next == null) break; 
-
             pathTotalCost += next.getCost();
             path.add(next);
-            randomVertex = next.getEnd();
-            visited[randomVertex] = true;
+            current = next.getStart();
+            visited[current] = true;
         }
-        return path;
+        HashMap<List<Edge>, Double> pathAndCost = new HashMap<>();
+        pathAndCost.put(path, pathTotalCost);
+        return pathAndCost;
     }
 
     // condition for if visited
@@ -65,41 +85,73 @@ public class AntColonyOptimization {
         return false;
     }
 
-    private double[] getProbabilities(int currentVertex, AdjacencyListGraph G, boolean[] visited, double a, double b) {
-        List<Edge> edges = G.getEdges(currentVertex); 
-        List<Double> probabilitiesList = new ArrayList<>();
-
+    private HashMap<Edge, Double> getProbabilities(int currentVertex, AdjacencyListGraph G, boolean[] visited, double a, double b) {
+        Bag<Integer> edgesInts = G.getAdj()[currentVertex];
+        List<Edge> edges = findEdges(G, edgesInts, currentVertex);
+        HashMap<Edge, Double> probabilitiesMap = new HashMap<>();
         double sum = 0.0; 
-        
         for (Edge edge : edges) {
             if (!visited[edge.getEnd()]) { 
                 double heuristic = 1.0 / edge.getCost(); 
                 double pheromoneLevel = edge.getPheromoneLevel();
                 double numerator = Math.pow(pheromoneLevel, a) * Math.pow(heuristic, b);
                 double denominator = Math.pow(pheromoneLevel, a) + Math.pow(heuristic, b);
-
                 double probability = numerator / denominator; 
-                probabilitiesList.add(probability);
+                probabilitiesMap.put(edge, probability);
                 sum += probability;
             }
         }
         //normalizing
-        double[] probabilities = new double[probabilitiesList.size()];
-        for (int i = 0; i < probabilities.length; i++) {
-            probabilities[i] = probabilitiesList.get(i) / sum;
+        for (Edge edge : probabilitiesMap.keySet()) {
+            double currProb = probabilitiesMap.get(edge);
+            currProb = probabilitiesMap.get(edge) / sum;
+            probabilitiesMap.replace(edge, currProb);
         }
-        return probabilities;
+        return probabilitiesMap;
     }
 
-      
-    // private Edge chooseNextVertex(double[] probabilities) {
-    // }
+    // code to choose probability modified from https://stackoverflow.com/questions/9330394/how-to-pick-an-item-by-its-probability
+    private Edge chooseNextVertex(HashMap<Edge, Double> probabilities) {
+        double totalSum = 0.0;
+        for (double probability : probabilities.values()) {
+            totalSum += probability;
+        }
+        double index = rand.nextDouble(totalSum);
+        double sum = 0;
+        double chosenProb = 0;
+        for (double probability : probabilities.values()) {
+            if(sum < index) {
+                sum += probability;
+                index++;
+            } else {
+                chosenProb = probability;
+                break;
+            }
+        }
+        for (Entry<Edge, Double> entry : probabilities.entrySet()) {
+            if (Objects.equals(chosenProb, entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
 
-    public void updatePheromones(AdjacencyListGraph G, double rho) {
+    private void updatePheromones(AdjacencyListGraph G, double rho) {
         for (Edge edge : G.getEdges()) { 
             edge.setPheromoneLevel((1 - rho) * edge.getPheromoneLevel());
         }
     }
-        
-    
+
+    private List<Edge> findEdges(AdjacencyListGraph G, Bag<Integer> edgesInts, int currentVertex) {
+        List<Edge> edges = new ArrayList<>();
+        List<Edge> allEdges = G.getEdges();
+        while (edgesInts.iterator().hasNext()) {
+            for (Edge e : allEdges) {
+                if (e.getStart() == currentVertex) {
+                    edges.add(e);
+                }
+            }
+        }
+        return edges;
+    }
 }
